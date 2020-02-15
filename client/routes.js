21dbea22f9fcbc4ac160dc59df9a2f6f6e81da81
John Tieu <johnson.tieu@gmail.com>
2020-02-14 21:38:07 -0500

    first commit

--- client/routes.js
+++ client/routes.js
@@ -15,7 +15,7 @@ class Routes extends Component {
 
   render() {
     const {isLoggedIn} = this.props
-
+    console.log('hello')
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
